6fdc3a6714c603eb6812589a3b4b54bb70dfb930
carlos <cbdev2021@gmail.com>
2023-07-10 04:06:00 -0400

    cors 1

--- server.js
+++ server.js
@@ -30,6 +30,18 @@ app.use(
 // };
 // app.use(cors(corsConfig));
 
+app.use(function(req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "*");
+  const allowedOrigins = ['http://localhost:4200', 'http://gamebrag.onrender.com', 'https://gamebrag.onrender.com'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+  next();
+});
 
 
 // parse requests of content-type - application/json
