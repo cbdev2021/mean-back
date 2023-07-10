41e09e715702cfc282319d89725ab70b25933a23
carlos <cbdev2021@gmail.com>
2023-07-10 03:37:08 -0400

    cors funciona2

--- server.js
+++ server.js
@@ -19,7 +19,8 @@ const app = express();
 app.use(
    cors({
      credentials: true,
-     origin: ["http://localhost:4200"],
+     //origin: ["http://localhost:4200"],
+     origin: '*'
    })
 );
  
